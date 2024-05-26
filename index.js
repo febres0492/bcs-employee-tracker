01f6d3e20173483e8e7f21d3270afbe4678d4e60
Carlos Febres <febres0492@gmail.com>
2024-05-25 22:42:24 -0400

    Changing Inquirer version

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const c = require('./lib/utils').c
 const { Pool } = require('pg');
 const PORT = process.env.PORT || 3001;
 
+// Connect to the database
+const pool = new Pool(
+    {
+      user: 'postgres',
+      password: '0492',
+      host: 'localhost',
+      database: 'departments',
+    },
+    console.log(`Connected to the departments database.`)
+)
+
+// defining inquirer questions
 const questions = [
     {   name: 'options', type:'list', message: `Options:\n`,
         choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
     }
 ]
 
-const pool = new Pool(
-    {
-      user: 'postgres',
-      password: '0492',
-      host: 'localhost',
-      database: 'department_db',
-    },
-    console.log(`Connected to the employes' database.`)
-)
+// defining inquirer questions for adding a department
+const addDepartmentQuestions = [
+    {   name: 'department', type:'input', message: `Enter the department name: ` }
+]
+
