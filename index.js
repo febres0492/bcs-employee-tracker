c8bf5f9b4495bd6248cb4e73717ac07002674b77
Carlos Febres <febres0492@gmail.com>
2024-05-28 18:11:11 -0400

    Update: - Adding following questions. - Refactoring CreateTable function;

--- index.js
+++ index.js
@@ -12,6 +12,38 @@ const databaseConfig = {
     port: 5432,
 }
 
+// defining inquirer questions
+const questions = {
+    'initialQuestion': [
+        {   name: 'options', type:'list', message: `Options:`,
+            choices: [ 'View all departments', 'View all roles', 'View all employees', 
+                'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', c('Exit program\n','y')
+            ],
+        }
+    ],
+    'Add a department': [
+        { name: 'name', type: 'input', message: 'Enter department name:' }
+    ],
+    'Add an employee': [
+        { name: 'first_name', type: 'input', message: 'Enter first name:' },
+        { name: 'last_name', type: 'input', message: 'Enter last name:' },
+        { name: 'role_id', type: 'input', message: 'Enter role id:' },
+        { name: 'manager_id', type: 'input', message: 'Enter manager id:' },
+    ],
+    'Add a role': [
+        { name: 'title', type: 'input', message: 'Enter role title:' },
+        { name: 'salary', type: 'input', message: 'Enter salary:' },
+        { name: 'department_id', type: 'input', message: 'Enter department id:' },
+    ],
+    'Update an employee role': [
+        { name: 'employee_id', type: 'input', message: 'Enter employee id:' },
+        { name: 'role_id', type: 'input', message: 'Enter role id:' },
+    ],
+    'Exit program': [
+        { name: 'exit', type: 'list', message: `${c('End execution?\n','y')}`, choices: ['yes', 'no'] }
+    ]
+}
+
 const options = {
     'Add a department': `INSERT INTO department (name) VALUES ('Engineering');`,
     'Add an employee': `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Doe', 1, 1);`,
@@ -22,21 +54,12 @@ const options = {
     'View all employees': `SELECT * FROM employee;`,
 }
 
-// defining inquirer questions
-const questions = [
-    {   name: 'options', type:'list', message: `Options:\n`,
-        choices: [ 'View all departments', 'View all roles', 'View all employees', 
-            'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'
-        ],
-    }
-]
-
 async function main(){
     let pool = new Pool(databaseConfig)
 
     // checking if database exists
     let newDbName = 'employee_db'
-    U.validateDB(pool, newDbName)
+    await U.validateDB(pool, newDbName)
 
     // connecting to the new database
     pool = new Pool({...databaseConfig, database: newDbName})
@@ -44,15 +67,33 @@ async function main(){
     // creating tables
     await U.createTable(pool)
 
-    const answers = await inquirer.prompt(questions)
-    const query = options[answers.options]
-    const res = U.processQuery(pool, query)
+    let curQuestion = questions['initialQuestion']
+    let done = false
+    while (done == false) {
+        const answers = await inquirer.prompt(curQuestion)
+
+
+
+        // const query = options[answers.options]
+        // const res = await U.processQuery(pool, query)
+        console.log(c(res.rows))
+
+        // end the loop if the user selects the initial question
+        if(curQuestion != questions['initialQuestion']){
+            done = true
+        }
+    }
+
+    const answers = await inquirer.prompt(questions['initialQuestion'])
+    // const query = options[answers.options]
+    
+    const res = await U.processQuery(pool, query)
 
     console.log(c(res.rows))
-    console.time('Time taken')
 
-    pool.end()
-    console.timeEnd('Time taken')
+    await pool.end()
 
+    // this is just to stop the process. sometimes it takes over 5 seconds to stop
+    process.exit(0)
 }
 main()
