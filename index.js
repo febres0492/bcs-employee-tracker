0827170f356901be9a40e731fecd232d8006f554
Carlos Febres <febres0492@gmail.com>
2024-05-28 22:34:42 -0400

    Update

--- index.js
+++ index.js
@@ -12,16 +12,29 @@ const databaseConfig = {
     port: 5432,
 }
 
+
 // defining inquirer questions
-const questions = {
+const exitText = c('Exit program?\n', 'y')
+const inquirerQuestions = {
     'initialQuestion': [
         {   name: 'options', type:'list', message: `Options:`,
             choices: [ 'View all departments', 'View all roles', 'View all employees', 
-                'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', c('Exit program\n','y')
+                'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 
+                'Remove a department', 'Remove a role', 'Remove an employee', 'Remove an employee role',
+                exitText
             ],
         }
     ],
     'Add a department': [
+        { name: 'name', type: 'input', message: 'Enter department name:', validate: function(value) {
+            if (value.length) {
+                return true;
+            } else {
+                return 'Please enter a department name';
+            }
+        }}
+    ],
+    'Remove a department': [
         { name: 'name', type: 'input', message: 'Enter department name:' }
     ],
     'Add an employee': [
@@ -39,13 +52,14 @@ const questions = {
         { name: 'employee_id', type: 'input', message: 'Enter employee id:' },
         { name: 'role_id', type: 'input', message: 'Enter role id:' },
     ],
-    'Exit program': [
-        { name: 'exit', type: 'list', message: `${c('End execution?\n','y')}`, choices: ['yes', 'no'] }
-    ]
+    // [exitText]: [
+    //     { name: 'exit', type: 'list', message: `${c('End execution?\n','y')}`, choices: ['yes', 'no'] }
+    // ]
 }
 
-const options = {
-    'Add a department': `INSERT INTO department (name) VALUES ('Engineering');`,
+const queryOptions = {
+    'Add a department': `INSERT INTO department (name) VALUES ('[name]');`,
+    'Remove a department': `DELETE FROM department WHERE name = '[name]';`,
     'Add an employee': `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Doe', 1, 1);`,
     'Add a role': `INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 90000, 1);`,
     'Update an employee role': `SELECT * FROM employee;`,
@@ -67,20 +81,49 @@ async function main(){
     // creating tables
     await U.createTable(pool)
 
-    let curQuestion = questions['initialQuestion']
-    let done = false
-    while (done == false) {
+    let continueExecution = true
+    let curQuestion = inquirerQuestions['initialQuestion']
+    let query = 'View all departments'
+
+    const exiting = async () => {
+        continueExecution = false
+        console.log('Exiting program...')
+        process.exit(0)
+    }
+
+    while (continueExecution == true) {
+
         const answers = await inquirer.prompt(curQuestion)
 
 
+        // checking if the user wants to exit the program
+        if(answers.options ==  exitText){ exiting() }
 
-        // const query = options[answers.options]
-        // const res = await U.processQuery(pool, query)
-        console.log(c(res.rows))
+        // checking if answer is a key in inquirerQuestions
+        if(Object.keys(inquirerQuestions).includes(answers.options)){
+            curQuestion = inquirerQuestions[answers.options]
+            query = queryOptions[answers.options]
+            continue
+        } 
+        
 
-        // end the loop if the user selects the initial question
-        if(curQuestion != questions['initialQuestion']){
-            done = true
+        if(curQuestion == inquirerQuestions['initialQuestion']){ 
+            query = queryOptions[answers.options]
+        }else{
+            query = U.replacingPlaceHolders(query, answers )
+        }
+
+        // if(answers.options.indexOf('Remove') > -1){ 
+        //     query = U.replacingPlaceHolders(query, answers )
+        //     debugger
+        // }
+
+        const res = await U.processQuery(pool, query)
+        console.log(c(res.rows),'\n')
+
+        // resetting the question to the initial question
+        if(curQuestion != inquirerQuestions['initialQuestion']){
+            curQuestion = inquirerQuestions['initialQuestion']
         }
     }
 
